44bed9c92e4f18b97d37d0014a44efb0515196a5
mahamjabbar456 <mahamjabbar320@gmail.com>
2024-03-22 12:37:41 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+"use strict";
+// Question 7 and 8
+console.log(6 + 2); //addition
+console.log(10 - 2); //subtraction
+console.log(4 * 2); //multiplication
+console.log(16 / 2); //division
+// Question 9
+let FavNumber = 14;
+let message = `My favourite number is ${FavNumber}`;
+console.log(message);
